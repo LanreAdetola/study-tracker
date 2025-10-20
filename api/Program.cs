38f9375c8cd2e9e32fe358d48a9d1adc92be5a96
LanreAdetola <100709089+LanreAdetola@users.noreply.github.com>
2025-10-20 18:09:39 +0200

    Test: Minimal Azure Functions API to isolate deployment issues

--- api/Program.cs
+++ api/Program.cs
@@ -1,26 +1,12 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Azure.Cosmos;
-using StudyTracker.Api.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
-        // Configure Cosmos DB client
-        services.AddSingleton<CosmosClient>(serviceProvider =>
-        {
-            var connectionString = Environment.GetEnvironmentVariable("CosmosDBConnectionString");
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                throw new InvalidOperationException("CosmosDBConnectionString environment variable is required");
-            }
-            return new CosmosClient(connectionString);
-        });
-        
-        // Add study session service
-        services.AddScoped<IStudySessionService, StudySessionService>();
+        // Minimal configuration for testing deployment
     })
     .Build();
 
